63e87149577d8eedf90698304527221f1c8b0792
Vivek Kumar <kumar97vivek@gmail.com>
2023-07-01 18:52:31 +0530

    Upgrade old code and dependencies

--- src/components/Navbar/MenuItems.js
+++ src/components/Navbar/MenuItems.js
@@ -1,42 +1,15 @@
 
 export const PrimaryMenuItems = [
     {
-        title: 'Dummy Website',
-        url: '/dummy-website',
-        cName: 'nav-link',
+        title: 'Home',
+        url: '/',
     },
     {
         title: 'More',
         url: '/more',
-        cName: 'nav-link',
     },
     {
         title: 'About Me',
         url: '/about',
-        cName: 'nav-link',
-    },
-]
-
-
-export const DummyWebsiteMenuItems = [
-    {
-        title: 'Home',
-        url: '/dummy-website',
-        cName: 'nav-link',
-    },
-    {
-        title: 'Demo2',
-        url: '/demo2',
-        cName: 'nav-link',
-    },
-    {
-        title: 'Demo3',
-        url: '/demo3',
-        cName: 'nav-link',
-    },
-    {
-        title: 'Demo4',
-        url: '/dummy-website/about',
-        cName: 'nav-link',
     },
 ]
